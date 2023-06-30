1203a8e4aec7faf14a7fd01f06a72f4ba96ee43b
ninejuan <ninejuan@protonmail.com>
2023-07-01 01:09:44 +0900

    inital commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+/**
+ * @author Juan Lee
+ * @since 2023-07-01
+ */
+
+export default {
+    /* Initalize Function Start */
+    /**
+     * @param {Object} mail
+     * @param {Object} web
+     */
+    init: function initalize(mail, web) {
+        console.log(mail.ar, web)
+    }
+}
